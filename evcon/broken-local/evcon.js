c4662b1cd2ac75687cba868ec1d1b104f3a8c3b7
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-05-16 15:43:57 -0300

    Adding good and broken local versions

---
+++ evcon/broken-local/evcon.js
@@ -0,0 +1,42 @@
+var db = require('monk')('localhost/evcon');
+
+var accounts = db.get('accounts');
+var transactions = db.get('transactions');
+
+function atRandom(list) {
+  var position = Math.round(Math.random() * (list.length - 1));
+  return list[position]
+}
+
+function updateBalance(id, delta) {
+  var balance = accounts.find({_id: id})[0].balance
+  accounts.update({_id:id}, {balance: delta + balance});
+}
+
+function recordTransaction(accountId, amount) {
+  transactions.insert({accountId: accountId, amount: amount});
+}
+
+var amountProcessed =  0;
+
+var deltas = [1, -1, 100, 2000, 300, -10, -600, -25, 150, -40]
+
+function withAmount(amount, f) {
+  f(amount);
+  amountProcessed += amount;
+}
+
+var interval = setInterval(function() {
+  withAmount( atRandom(deltas), function(amount){
+    console.log('Perform op with amount '+ amount );
+    updateBalance(1, amount);
+    recordTransaction(1, amount);
+  })
+}, 100);
+
+process.on('SIGINT', function() {
+  clearInterval(interval);
+  console.log("Amount processed " + amountProcessed);
+  process.exit(0);
+});
+
